307d82fbf2601bc7ef7b0db0c008b79db366f1d8
Bocheng Zhou <bzou24@wisc.edu>
2024-06-12 02:50:29 -0500

    Add get_i

--- evaluate_clip_score.py
+++ evaluate_clip_score.py
@@ -9,10 +9,17 @@ import io
 import torch
 import tqdm
 import os
+import argparse
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 # model, preprocess = clip.load('ViT-B/32', download_root="/staging/bzou24/checkpoints", device=device)
 model, preprocess = longclip.load("/staging/bzou24/checkpoints/longclip-L.pt", device=device)
+
+def default_argument_parser():
+    parser = argparse.ArgumentParser(description="convert json to spreadsheet")
+    parser.add_argument(
+        "--format", choices=["svg", "tikz", "graphviz"], default="", required=True, help="the format of the vector graphics")
+    parser.add_argument("--png-path", required=True)
 
 def evaluate_sim_img_text(img: Image, caption: str):
     image_processed = preprocess(img).unsqueeze(0).to(device)
@@ -27,16 +34,14 @@ def evaluate_sim_img_text(img: Image, caption: str):
 
 
 def main():
+    args = default_argument_parser().parse_args()
     # svg_path = "data/svg-gen/generated_svgs.json"
-    png_path = "data/tikz-gen/sampled_pngs"
-    caption_path = "data/tikz-gen/captions.json"
+    caption_path = "data/%s-gen/captions.json"%args.format
     # svg_data = json.load(open(svg_path))
     caption_data = json.load(open(caption_path))
     clip_score = 0
     for key in tqdm.tqdm(caption_data.keys()):
-        # png_bytes = cairosvg.svg2png(bytestring=svg_data[key], background_color="white")
-        # image = Image.open(io.BytesIO(png_bytes))
-        image = Image.open(os.path.join(png_path, key))
+        image = Image.open(os.path.join(args.png_path, key))
         caption = caption_data[key]
         clip_score += evaluate_sim_img_text(image, caption)
     print(clip_score/len(caption_data.keys()))
---
+++ get_i.py
@@ -0,0 +1,23 @@
+import utils
+import json
+import os
+import argparse
+import cairosvg
+import PIL.Image
+import io
+import tqdm
+
+def default_argument_parser():
+    parser = argparse.ArgumentParser(description="render the original vector graphics in the dataset")
+    parser.add_argument("--dataset-path", required=True)
+    parser.add_argument("--idx", type=int, required=True)
+    return parser
+
+
+def main():
+    args = default_argument_parser().parse_args()
+    dataset = json.load(open(args.dataset_path))
+    print(dataset[args.idx])
+
+if __name__ == '__main__':
+    main()
